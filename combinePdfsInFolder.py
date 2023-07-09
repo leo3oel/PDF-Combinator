bb9af72b02059d41566359d562edf11816449ff0
leo3oel <spammailspam@aol.de>
2023-07-09 12:24:46 +0200

    use absolute paths

--- combinePdfsInFolder.py
+++ combinePdfsInFolder.py
@@ -3,10 +3,10 @@ import os
 
 if __name__ == "__main__":
     
-    itemsInFolder = os.listdir()
+    itemsInFolder = os.listdir(os.getcwd())
     filesInFolder = [item for item in itemsInFolder if os.path.isfile(item)]
-    pdfFiles = sorted([file for file in filesInFolder if file.endswith('.pdf')])
+    pdfFiles = sorted([os.path.join(os.getcwd(), file) for file in filesInFolder if file.endswith('.pdf')])
 
     if len(pdfFiles) > 1:
-        PdfCombinator = PdfCombinator(pdfFiles[0], pdfFiles[1], "out.pdf")
+        PdfCombinator = PdfCombinator(pdfFiles[0], pdfFiles[1], os.path.join(os.getcwd(), "out.pdf"))
         PdfCombinator.outputPdf()
